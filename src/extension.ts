c1925510acfcd395769d9721fd18c83f4d8076b0
ianandhum <38727945+ianandhum@users.noreply.github.com>
2024-07-29 09:37:13 +0530

    refactor: formatting

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,9 @@ export async function activate(context: vscode.ExtensionContext) {
 	};
 	await initProtols();
 
-	vscode.window.onDidChangeActiveTextEditor((e) => {
-		if (e === undefined) {
-			return;
-		}
-		if (!isProtolsStarted() && e.document.languageId === "proto3"){
-			initProtols();
+	vscode.window.onDidChangeActiveTextEditor(async (e) => {
+		if (!isProtolsStarted() && e?.document.languageId === "proto3") {
+			await initProtols();
 		}
 	});
 }
--- src/protols.ts
+++ src/protols.ts
@@ -14,8 +14,8 @@ const PROTOLS_BIN = "protols";
 const DOCUMENTATION_URI = "https://github.com/ianandhum/vscode-protobuf-support?tab=readme-ov-file#requirements";
 
 interface ProtolsCommand {
-    command: string
-    args?: string[]
+	command: string
+	args?: string[]
 }
 
 var disableWarnings = false;
@@ -23,63 +23,63 @@ let client: LanguageClient;
 
 
 async function showProtolsWarning(): Promise<void> {
-    let openDocsAction = "Open Documentation";
-    let dontShowAgainAction = "Dont Show Again";
+	let openDocsAction = "Open Documentation";
+	let dontShowAgainAction = "Dont Show Again";
 
-    let selectedAction = await vscode.window.showWarningMessage(
-        `Protols language server is not installed, language features are not enabled.
+	let selectedAction = await vscode.window.showWarningMessage(
+		`Protols language server is not installed, language features are not enabled.
         Install protols using "cargo install protols" to enable language features`,
-        openDocsAction,
-        dontShowAgainAction);
+		openDocsAction,
+		dontShowAgainAction);
 
-    switch (selectedAction) {
-        case openDocsAction:
-            vscode.env.openExternal(vscode.Uri.parse(DOCUMENTATION_URI));
-            break;
-    
-        case dontShowAgainAction: 
-            disableWarnings = true;
-    }
+	switch (selectedAction) {
+		case openDocsAction:
+			vscode.env.openExternal(vscode.Uri.parse(DOCUMENTATION_URI));
+			break;
+
+		case dontShowAgainAction:
+			disableWarnings = true;
+	}
 }
 
-export async function getProtolsCommand() : Promise<false | ProtolsCommand> {
-    let protolsPath: string = vscode.workspace.getConfiguration(PROTOLS_CONFIG_PATH).get("path") || PROTOLS_BIN;
+export async function getProtolsCommand(): Promise<false | ProtolsCommand> {
+	let protolsPath: string = vscode.workspace.getConfiguration(PROTOLS_CONFIG_PATH).get("path") || PROTOLS_BIN;
 
-    if (protolsPath === PROTOLS_BIN) {
-        let binPath = await which(PROTOLS_BIN, { nothrow: true });
-        if (binPath === null) {
-            await showProtolsWarning();
-            return false;
-        }
-    } else {
-         let binExists = fs.existsSync(protolsPath);
-         let isExecutable: boolean = false;
-         
-         if (binExists) {
-            try {
-                fs.accessSync(protolsPath, fs.constants.X_OK);
-                isExecutable = true;
-            } catch {
-                isExecutable = false;
-            }
-         }
+	if (protolsPath === PROTOLS_BIN) {
+		let binPath = await which(PROTOLS_BIN, { nothrow: true });
+		if (binPath === null) {
+			await showProtolsWarning();
+			return false;
+		}
+	} else {
+		let binExists = fs.existsSync(protolsPath);
+		let isExecutable: boolean = false;
 
-         if(!binExists || !isExecutable) {
-            await showProtolsWarning();
-            return false;
-         }
-    }
+		if (binExists) {
+			try {
+				fs.accessSync(protolsPath, fs.constants.X_OK);
+				isExecutable = true;
+			} catch {
+				isExecutable = false;
+			}
+		}
 
-    let protolsArgs:string[] = vscode.workspace.getConfiguration(PROTOLS_CONFIG_PATH).get("args") || [];
+		if (!binExists || !isExecutable) {
+			await showProtolsWarning();
+			return false;
+		}
+	}
 
-    return {
-        command: protolsPath,
-        args: protolsArgs
-    };
+	let protolsArgs: string[] = vscode.workspace.getConfiguration(PROTOLS_CONFIG_PATH).get("args") || [];
+
+	return {
+		command: protolsPath,
+		args: protolsArgs
+	};
 }
 
 export function startProtols(cmd: ProtolsCommand) {
-    const serverOptions: ServerOptions = {
+	const serverOptions: ServerOptions = {
 		run: {
 			command: cmd.command,
 			args: cmd.args,
@@ -110,12 +110,12 @@ export function startProtols(cmd: ProtolsCommand) {
 }
 
 export function stopProtols(): Thenable<void> | undefined {
-    if (!client) {
+	if (!client) {
 		return undefined;
 	}
 	return client.stop();
 }
 
 export function isProtolsStarted(): boolean {
-    return !!client;
+	return !!client;
 }
